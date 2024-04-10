5cdb7c23ad3e8bda195f67f91573ae1232923153
Thomaz Peres <thomaz.peres21@hotmail.com>
2024-04-09 23:36:12 -0300

    Almost done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use speedy2d::window::WindowHandler;
+use vector::Vector;
+
 fn main() {
     println!("Hello, world!");
 }
 
 struct Pendulum {
+    // Position of the pendulum
+    origin: Vector,
+
+    // Position of the ball
+    position: Vector,
+
     // The pendulum angle
     angle: f32,
 
@@ -15,36 +24,72 @@ struct Pendulum {
 }
 
 impl Pendulum {
-    fn new() {
-        
+    fn new(x: f32, y: f32, r: f32) -> Pendulum {
+        Pendulum {
+            origin: Vector::new(x, y),
+            position: Vector::new(0.0, 0.0),
+            angle: 1.0,
+            angular_velocity: (0.0),
+            angular_acceleration: (0.0),
+            r: (r),
+            m: 1.0,
+            g: 1.5,
+        }
     }
 
-    fn update() {
+    fn update(&mut self) {
+        // Pendulum equation to calculate the angular acceleration
+        self.angular_acceleration = -1.0 * self.g * self.angle.sin() / self.r;
 
+        // The angular velocity is the angular velocity plus the angular acceleration
+        self.angular_velocity += self.angular_acceleration;
+
+        // The angle is the angle plus the angular velocity
+        self.angle += self.angular_velocity;
+
+        // The position is the polar coordinates translated to cartesian coordinates.
+        self.position
+            .set(self.r * self.angle.sin(), self.r * self.angle.cos());
+
+        // The final position of the ball in the canvas is the origin of the pendulum plus the position vector
+        self.position.add(&self.origin);
     }
 
-    fn draw() {
-
-    }
+    fn draw() {}
 }
 
 mod vector {
-    struct Vector {
+    pub struct Vector {
         pub x: f32,
         pub y: f32,
     }
 
     impl Vector {
-        fn new() {
-            
+        pub fn new(x: f32, y: f32) -> Vector {
+            Vector { x, y }
         }
 
-        fn add() {
-            
+        pub fn add(&mut self, other: &Vector) -> &Vector {
+            self.x += other.x;
+            self.y += other.y;
+            self
         }
 
-        fn set() {
-            
+        pub fn set(&mut self, x: f32, y: f32) -> &Vector {
+            self.x = x;
+            self.y = y;
+            self
         }
+    }
+}
+
+struct MyWindowHandler {}
+
+impl WindowHandler for MyWindowHandler {
+    fn on_draw(
+        &mut self,
+        helper: &mut speedy2d::window::WindowHelper<()>,
+        graphics: &mut speedy2d::Graphics2D,
+    ) {
     }
 }
